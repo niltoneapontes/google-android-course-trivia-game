7fe5b5462852d672fd91737046a2c5e9da737847
Nilton <niltoneapontes@gmail.com>
2023-05-31 16:55:35 -0300

    feat: create and apply navgraph

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -15,6 +16,9 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
