e135b1e76a1aeaa2565f1a5f4fa0ff4fd53a12d3
Nilton <niltoneapontes@gmail.com>
2023-05-31 16:57:10 -0300

    refactor: simplify onclick listener navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,9 +17,9 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        binding.playButton.setOnClickListener {
-            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+                )
         return binding.root
     }
 }
