c5494a67275bc87ceb43d0564fcb503398d26a41
Nilton <niltoneapontes@gmail.com>
2023-05-31 16:56:26 -0300

    refactor: navigation action

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -17,7 +18,7 @@ class TitleFragment : Fragment() {
     ): View {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
         binding.playButton.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
         return binding.root
     }
